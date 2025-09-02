f2006a2c32db814c4d57367e8f0033b7da212f42
Pedro Oliveira <pedro_oliva@outlook.com.br>
2025-09-02 11:18:39 -0300

    input type

--- 01-graphql-course/src/graphql/post/resolvers.js
+++ 01-graphql-course/src/graphql/post/resolvers.js
@@ -1,12 +1,13 @@
-const posts = async (_, __, { getPosts }) => {
-  const posts = await getPosts();
-  return posts.json();
-}
-
 const post = async (_, { id }, { getPosts }) => {
   const response = await getPosts('/' + id);
   const post = await response.json();
   return post;
+}
+
+const posts = async (_, { input }, { getPosts }) => {
+  const apiFiltersInput = new URLSearchParams(input);
+  const response = await getPosts('/?' + apiFiltersInput);
+  return response.json();
 }
 
 export const postResolvers = {
