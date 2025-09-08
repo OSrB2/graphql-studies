22a840eb697fa18edf72931f869021474d3bf881
Pedro Oliveira <pedro_oliva@outlook.com.br>
2025-09-08 16:33:50 -0300

    Union Type

--- 01-graphql-course/src/graphql/post/resolvers.js
+++ 01-graphql-course/src/graphql/post/resolvers.js
@@ -1,14 +1,23 @@
 const post = async (_, { id }, { getPosts }) => {
   const response = await getPosts('/' + id);
+
+  if (!response.ok) {
+    return {
+      statusCode: 404,
+      message: 'Post not found',
+    };
+  }
+
   const post = await response.json();
+
   return post;
-}
+};
 
 const posts = async (_, { input }, { getPosts }) => {
   const apiFiltersInput = new URLSearchParams(input);
   const response = await getPosts('/?' + apiFiltersInput);
   return response.json();
-}
+};
 
 export const postResolvers = {
   Query: { post, posts },
@@ -16,6 +25,13 @@ export const postResolvers = {
     unixTimestamp: ({ createdAt }) => {
       const timestamp = new Date(createdAt).getTime() / 1000;
       return Math.floor(timestamp);
-    }
-  }
-}
+    },
+  },
+  PostResult: {
+    __resolveType: (obj) => {
+      if (typeof obj.statusCode !== 'undefined') return 'PostNotFoundError';
+      if (typeof obj.id !== 'undefined') return 'Post';
+      return null;
+    },
+  },
+};
