7c01e00ef87dfba6fa45a6892025d8651bf51191
Pedro Oliveira <pedro_oliva@outlook.com.br>
2025-09-09 10:41:10 -0300

    Relations and the N+1 problem

---
+++ 02-graphql-course/src/graphql/user/resolvers.js
@@ -0,0 +1,15 @@
+const users = async (_, { input }, { getUsers }) => {
+  const apiFiltersInput = new URLSearchParams(input);
+  const users = await getUsers('/?' + apiFiltersInput);
+  return users.json();
+};
+
+const user = async (_, { id }, { getUsers }) => {
+  const response = await getUsers('/' + id);
+  const user = await response.json();
+  return user;
+};
+
+export const userResolvers = {
+  Query: { user, users },
+};
